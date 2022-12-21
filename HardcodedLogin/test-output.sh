58c90ae725011629d56225b9eb10400e6bc32492
GregLigh97 <greg2lightbody@gmail.com>
2022-12-21 17:42:34 +0000

    Saved progress on FizzBuzz 2022-12-21 17:42:34 +0000

--- HardcodedLogin/test-output.sh
+++
@@ -1,7 +0,0 @@
-
--------------------------------------------------------
- T E S T S
--------------------------------------------------------
-Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
-Running HardcodedLoginTest
-What is your Username
