5f023a9df845019ff4d07fe91aa143fbcf4f2637
GregLigh97 <greg2lightbody@gmail.com>
2023-01-20 06:55:40 +0000

    Saved progress on Constructors 2023-01-20 06:55:40 +0000

--- Constructors/src/main/java/Player.java
+++ Constructors/src/main/java/Player.java
@@ -31,7 +31,7 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-        this.playerName = "Frodo";
+        this.playerName = playerName;
         this.level = 1;
 
     }
@@ -50,6 +50,8 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
+        this.playerName = playerName;
+        this.level = level;
 
     }
 
