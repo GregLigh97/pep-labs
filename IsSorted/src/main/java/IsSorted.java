c46f8cfd0181f10fb2454e86d6cf0de6396f1c73
GregLigh97 <greg2lightbody@gmail.com>
2023-01-20 23:36:02 +0000

    Saved progress on IsSorted 2023-01-20 23:36:02 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -9,11 +9,11 @@ public class IsSorted {
     public boolean checkSort(int[] arr){
         int i;
         for(i = 0; i < arr.length-1; i++){
-            if(arr[i-1] < arr[i + 1]){
-                return true;
+            if(arr[i] > arr[i + 1]){
+                return false;
             }   
             }
-            return false; 
+            return true; 
    }
 }
         
