067eb6f9bb62bad028bf05b4c85dc05348a8bcd6
GregLigh97 <greg2lightbody@gmail.com>
2023-01-24 20:31:10 +0000

    Saved progress on PigLatin 2023-01-24 20:31:10 +0000

--- PigLatin/src/main/java/PigLatin.java
+++ PigLatin/src/main/java/PigLatin.java
@@ -17,12 +17,7 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        in = "banana"; 
-        return "ananabay";
-        in = "pear";
-        return "earpay";
-        in = "pay";
-        return "eachpay";
+        return in.substring(1) + in.substring(0, 1) + "ay";
     }
 }
-}
+
