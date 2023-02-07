1c379c7296e9089005f22e3e0e355f85069c0c52
GregLigh97 <greg2lightbody@gmail.com>
2023-02-07 19:13:55 +0000

    Saved progress on Generic 2023-02-07 19:13:55 +0000

--- Interfaces/src/main/java/SampleClassC.java
+++ Interfaces/src/main/java/SampleClassC.java
@@ -24,9 +24,11 @@ public class SampleClassC implements Comparable<SampleClassC>{
      * CompareTo should return a negative value if this object's 'val' is less than another object's 'val',
      * and positive otherwise. If 'val' is equal for both objects, return 0.
      *
-     * @param other another object of type SampleClassC, which we are comparing to 'this' object.
+     * @param val another object of type SampleClassC, which we are comparing to 'this' object.
      */
-    public int compareTo(SampleClassC other) {
-       return val.compareTo(int val);
+    public int compareTo(val) {
+        if(val.compareTo(val))
+       return -1;
     }
+}
 }
