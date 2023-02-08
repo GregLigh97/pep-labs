cacd9abcc44da87bce68ca576be1f2a1e5c3f1f8
GregLigh97 <greg2lightbody@gmail.com>
2023-02-08 01:07:45 +0000

    Saved progress on Isogram 2023-02-08 01:07:45 +0000

--- WordCountMap/src/main/java/WordCountMap.java
+++ WordCountMap/src/main/java/WordCountMap.java
@@ -2,6 +2,8 @@
 import java.util.HashMap;
 import java.util.Map;
 public class WordCountMap {
+    private String str;
+
     /**
      * Receive a string of multiple words.
      * Return a Map object where the key is a word and the value is the count of occurrences of the word.
@@ -15,21 +17,17 @@ public class WordCountMap {
      * @return a Map (key/value pairs) of all the words mapped to their number of occurrences.
      */
     public Map<String, Integer> returnWordMap(String words){
-    }
         Map<String, Integer> hashMap = new HashMap<>();
-        private String str;
-        String[] words = str.split(" ");{
-        }
-    {
+        String[] st = str.split(" ");{
         for (String word : words) {
             Integer integer = hashMap.get(words);
- 
             if (integer == null)
                 hashMap.put(word, 1);
             else {
                 hashMap.put(word, integer + 1);
             }
     return hashMap;
-}
+    }
+  }
 }
 }
