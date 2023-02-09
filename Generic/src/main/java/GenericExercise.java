09bd8c9f043fe7902761a6d15595a2c932b1ae12
GregLigh97 <greg2lightbody@gmail.com>
2023-02-09 08:36:28 +0000

    Saved progress on CharComparison 2023-02-09 08:36:28 +0000

--- Generic/src/main/java/GenericExercise.java
+++ Generic/src/main/java/GenericExercise.java
@@ -14,12 +14,15 @@
  * @param <T> a placeholder for any type of object.
  */
 public class GenericExercise<T>{
-   
+    public T get() { 
+        return T; 
+    }
 
     /**
      * Set data to 'in'.
      * @param in an Object that will be of type T at runtime.
      */
+
     public void loadGenericItem(T in){
      GenericClass<in> obj = new GenericClass<>();
 
@@ -27,8 +30,9 @@ public class GenericExercise<T>{
     /**
      * @return T data. The type of Data will again be set when the GenericExercise object is created (at runtime).
      */
-    public T returnGenericItem(){
+    public Object returnGenericItem(){
+        return T;        
+        }
         
-        return T;
     }
-}
+
